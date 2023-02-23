9c5c86991fae65afe847f29a85b4125ca606f064
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-02-23 20:25:27 +0300

    challenge 2 solved

--- challenges.rb
+++ challenges.rb
@@ -9,11 +9,21 @@ def compare_numbers(a, b)
     else
       "#{a} is equal to #{b}"
     end
-  end
+end
 
 # Challenge2: Ordered Count of Characters(Toy Problem)
-def count_of_characters(str)
-    
+#Count the number of occurrences of each character and return it as a (list of arrays) in order of appearance. For empty output return (an empty list).
+def count_of_characters(input)
+  count = {}
+  input.each_char do |char|
+    if count[char]
+      count[char] += 1
+    else
+      count[char] = 1
+    end
+  end
+  result = count.sort_by { |k, v| input.index(k) }
+  result.map { |k, v| [k, v] }
 end
 
 #Tests for Challenge 1:
@@ -23,7 +33,7 @@ pp compare_numbers(2,2)
 pp compare_numbers(3,4)
 
 ##Tests for Challenge 2:
-pp count_of_characters("abracadabra")
-pp count_of_characters("Code Wars")
-pp count_of_characters("233312")
-pp count_of_characters("Serena")
+pp count_of_characters("abracadabra") #---->  [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
+pp count_of_characters("Code Wars") #-----> [['C', 1], ['o', 1], ['d', 1], ['e', 1], [' ', 1], ['W', 1], ['a', 1], ['r', 1], ['s', 1]]
+pp count_of_characters("233312") # [['2', 2], ['3', 3], ['1', 1 ]]
+pp count_of_characters("Serena") #[["S", 1]], ["e", 2], ["r", 1], ['n', 1], ['a', 1]
